c00494e6670a3225cad2a2a3728dcb074f883e01
LucasMochi <mochilucas8@gmail.com>
2024-07-01 20:56:33 -0300

    Primera entrega

---
+++ models/Sonda.js
@@ -0,0 +1,19 @@
+class Sonda {
+    
+    sondas = [];
+
+    add = async (sonda) => {
+        this.sondas.push(sonda);
+        return this.sondas[this.sondas.length - 1];
+    };
+
+    getAll = async () => {
+        return this.sondas
+    };
+
+    getById = async () => {
+        return sondas.filter(ele => ele.id === id);
+    };
+}
+
+export default Sonda;
---
+++ services/Service.js
@@ -0,0 +1,24 @@
+import Sonda from "../models/Sonda.js";
+import { validateSonda, validateId } from "../utils/validation.js"
+const sonda = new Sonda();
+
+class Service {
+
+    addSonda = (id, temperatura) => {
+        const sonda = { id, temperatura };
+        validateSonda(sonda);
+        return sonda.add(sonda);
+    };
+    
+    getAllSondas = () => {
+        return sonda.getAll();
+    };
+    
+    getSondaById = (id) => {
+        validateId(id);
+        return sonda.getById(id);
+    };
+};
+
+export default Service;
+
